c32bac0e3c7007350352ab7afdd52606f0ed5060
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-05-11 02:36:39 -0300

    Fixed cookie problem and added heartbeat

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -4,18 +4,24 @@ import API from "./api.js";
 const {AuthServiceClient} = require("./proto/auth_grpc_web_pb.js");
 const {AuthLoginRequest, AuthVoid} = require("./proto/auth_pb.js");
 
-const service = new AuthServiceClient(Config.APIAddress, null, null);
+const service = new AuthServiceClient(Config.APIAddress, null, {
+    'withCredentials': true
+});
 
 const AuthAPI = {
     Login: (data, onSuccess, onError) => {
         const request = new AuthLoginRequest();
-        request.Phone(data.phone);
+        request.setPhone(data.phone);
         request.setPassword(data.password);
         service.login(request, API.getMetadata(), (err, response) => API.handleResponse(err, onError, response, onSuccess));
     },
     Logout: (data, onSuccess, onError) => {
         const request = new AuthVoid();
         service.logout(request, API.getMetadata(), (err, response) => API.handleResponse(err, onError, response, onSuccess));
+    },
+    Heartbeat: (data, onSuccess, onError) => {
+        const request = new AuthVoid();
+        service.heartbeat(request, API.getMetadata(), (err, response) => API.handleResponse(err, onError, response, onSuccess));
     }
 }
 
