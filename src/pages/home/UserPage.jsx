650a7c20a01848e353daf082d3da1492d0f6824c
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-05-11 02:37:01 -0300

    Added update profile

--- src/pages/home/UserPage.jsx
+++ src/pages/home/UserPage.jsx
@@ -1,19 +1,22 @@
-import Check from "../../common/check.js";
-import {Button, Grid, Typography} from "@mui/material";
+import API from "../../api/api.js";
 import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import Error from "../../common/error.js";
 import ButtonOption from "../../components/ButtonOption.jsx";
-import CenterContainer from "../../components/CenterContainer.jsx";
+import ConfirmDialog from "../../components/ConfirmDialog.jsx";
 import FormField from "../../components/FormField.jsx";
 import OptionsColumn from "../../components/OptionsColumn.jsx";
 import PageHeader from "../../components/PageHeader.jsx";
 import {useAuthentication} from "../../providers/AuthProvider.jsx";
+import {useNotification} from "../../providers/NotificationProvider.jsx";
 
 const UserPage = () => {
     const navigate = useNavigate()
+    const notify = useNotification();
     const auth = useAuthentication();
     const user = auth.user || {};
+
+    const [confirmOpen, setConfirmOpen] = useState(false);
     const [form, setForm] = useState({
         name: {
             value: user.name,
@@ -56,16 +59,34 @@ const UserPage = () => {
     const handleSubmit = () => {
         const ok = validateInputs();
         if (ok) {
-            // TODO: implementar update
+            const formData = {
+                name: form.name.value,
+                phone: form.phone.value,
+                password: form.password.value,
+            }
+            API.User.Update(formData, (data) => {
+                console.log(data);
+                notify.show("", "Conta atualizada!", "success");
+            })
         }
     }
 
     const handleDelete = () => {
-        // TODO: implementar delete
+        const uuid = auth.user.uuid
+        API.User.Delete({uuid}, (data) => {
+            console.log(data);
+        })
     }
 
     return (
         <OptionsColumn>
+            <ConfirmDialog
+                openState={[confirmOpen, setConfirmOpen]}
+                danger
+                title="Excluir Conta"
+                text="Tem certeza que deseja excluir sua conta?"
+                onConfirm={handleDelete}
+            />
             <PageHeader variant={"h2"} text="Minha Conta"/>
             <FormField
                 label="Nome"
@@ -74,6 +95,7 @@ const UserPage = () => {
                 formState={[form, setForm]}
             />
             <FormField
+                disabled
                 label="Telefone"
                 type="text"
                 id="phone"
@@ -91,7 +113,7 @@ const UserPage = () => {
             />
             <ButtonOption
                 text="Excluir Conta"
-                onClick={handleDelete}
+                onClick={() => setConfirmOpen(true)}
             />
         </OptionsColumn>
     )
