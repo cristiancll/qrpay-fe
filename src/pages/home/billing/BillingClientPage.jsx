4db34b08313e1fd4f7c0086d08fc0b01e93414b0
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-05-18 05:07:44 -0300

    minor

--- src/pages/home/billing/BillingClientPage.jsx
+++ src/pages/home/billing/BillingClientPage.jsx
@@ -1,14 +1,12 @@
-import CircularProgress from "@mui/material/CircularProgress";
 import CssBaseline from "@mui/material/CssBaseline";
 import React, {useEffect, useState} from 'react';
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import API from "../../../api/api.js";
 import Auth from "../../../common/auth.js";
 import Utils from "../../../common/utils.js";
 import OutletContainer from "../../../components/OutletContainer.jsx";
 import {useAuthentication} from "../../../providers/AuthProvider.jsx";
 import BillingClientProvider from "../../../providers/BillingClientProvider.jsx";
-import {useNotification} from "../../../providers/NotificationProvider.jsx";
 import UnauthorizedPage from "../../UnauthorizedPage.jsx";
 import BillingClientBar from "./BillingClientBar.jsx";
 import BillingClientConfirmOrderDialog from "./BillingClientConfirmOrderDialog.jsx";
@@ -16,14 +14,14 @@ import CategoryCards from "./CategoryCards.jsx";
 
 const BillingClientPage = () => {
     const auth = useAuthentication()
-    const notify = useNotification()
+    const navigate = useNavigate()
     const [user, setUser] = useState({})
     const [searchParams, setSearchParams] = useSearchParams()
     const uuid = searchParams.get("uuid")
     useEffect(() => {
         API.User.Get({uuid},
             (res) => setUser(Utils.sanitizeProto(res.getUser())),
-            (err) => notify.show(err.message, "error")
+            (err) => navigate("/billing")
         )
     }, [uuid])
 
@@ -37,7 +35,6 @@ const BillingClientPage = () => {
         <BillingClientProvider user={user}>
             <OutletContainer rowSpacing={1} alignItems={"flex-start"} justifyContent={"space-between"}>
                 <BillingClientConfirmOrderDialog/>
-                <CssBaseline/>
                 <CategoryCards/>
                 <BillingClientBar/>
             </OutletContainer>
